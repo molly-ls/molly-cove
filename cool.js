238aeed688916ca90bd6a92c21a4df86eccff726
Molly Luo <molly@lawscout.ca>
2017-01-12 16:11:28 -0500

    - update

--- cool.js
+++ cool.js
@@ -1,6 +1,9 @@
 "use strict";
 var page = require('webpage').create();
 
+// phantom.setProxy('http://proxy-23-21-132-4.proximo.io','80','http', 'proxy','f79d1ac2e616-49c1-93ac-cb4d647136d7')
+// phantom.setProxy('167.114.167.221', '3128')
+
 page.settings.userAgent="Mozilla/5.0 (Macintosh; Intel Mac OS X) AppleWebKit/538.1 (KHTML, like Gecko) PhantomJS/2.1.1 Safari/538.1"
 page.open('https://www.ic.gc.ca/auth/app/scr/ic/idm/login?lang=eng&goto=https://www.nuans.com/auth/app/scr/corp/nuans/member/order.html', function(status) {
 	console.log(status)
@@ -9,10 +12,6 @@ page.open('https://www.ic.gc.ca/auth/app/scr/ic/idm/login?lang=eng&goto=https://
     return jQuery('title').text().trim()
   });
   console.log(title)
-  var test = page.evaluate(function() {
-  	return typeof WebTrends
-  })
-  console.log('WebTrends ->', test)
   phantom.exit();
 })
 
@@ -25,9 +24,9 @@ page.open('https://www.ic.gc.ca/auth/app/scr/ic/idm/login?lang=eng&goto=https://
 // 	}
 // };
 
-page.onResourceRequested = function (request) {
-    console.log('Request ' + JSON.stringify(request, undefined, 2));
-};
+// page.onResourceRequested = function (request) {
+//     console.log('Request ' + JSON.stringify(request, undefined, 1));
+// };
 
 page.onError = function (msg, trace) {
     console.log(msg);
