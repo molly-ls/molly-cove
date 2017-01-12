d85d807206838dd6f4ebfc3a1a60ee13d91065b7
Molly Luo <molly@lawscout.ca>
2017-01-12 13:53:51 -0500

    - test

--- cool.js
+++ cool.js
@@ -1,17 +1,6 @@
 "use strict";
 var page = require('webpage').create();
 
-// page.includeJs(
-//   // Include the https version, you can change this to http if you like.
-//   'https://www.ic.gc.ca/eic/home.nsf/js/webtrends.js',
-//   function() {
-//   	var foo = page.evaluate(function() {
-//   		return WebTrends
-//   	})
-//   	console.log(foo)
-//   }
-// );
-
 page.open('https://www.ic.gc.ca/auth/app/scr/ic/idm/login?lang=eng&goto=https://www.nuans.com/auth/app/scr/corp/nuans/member/order.html', function(status) {
 	console.log(status)
 	// console.log(page.injectJs('https://www.ic.gc.ca/eic/home.nsf/js/webtrends.js'))
@@ -26,35 +15,12 @@ page.open('https://www.ic.gc.ca/auth/app/scr/ic/idm/login?lang=eng&goto=https://
   phantom.exit();
 })
 
-page.onInitialized = function() {
-	// page.includeJs(
-	//   // Include the https version, you can change this to http if you like.
-	//   'https://www.ic.gc.ca/eic/home.nsf/js/webtrends.js',
-	//   function() {
-	//   	var foo = page.evaluate(function() {
-	//   		return WebTrends
-	//   	})
-	//   	console.log(foo)
-	//   }
-	// );
-	// console.log(page.injectJs('https://www.ic.gc.ca/eic/home.nsf/js/webtrends.js'))
-	if(page.injectJs('webtrends.js')) {
-		var test = page.evaluate(function() {
-	    return typeof WebTrends
-	  });
-	  console.log('onInitialized, webtrends-> ', test)
-	}
-};
-
-// page.onLoadStarted = function() {
-//   var currentUrl = page.evaluate(function() {
-//     return window.location.href;
-//   });
-//   console.log('Current page ' + currentUrl + ' will gone...');
-//   console.log('Now loading a new page...');
-// };
-
-// page.onUrlChanged = function(targetUrl) {
-//   console.log('New URL: ' + targetUrl);
+// page.onInitialized = function() {
+// 	if(page.injectJs('webtrends.js')) {
+// 		var test = page.evaluate(function() {
+// 	    return typeof WebTrends
+// 	  });
+// 	  console.log('onInitialized, webtrends-> ', test)
+// 	}
 // };
 
