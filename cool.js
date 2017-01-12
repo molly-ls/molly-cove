094602712bbab4665be2f47c8991ee20bd6490c6
Molly Luo <molly@lawscout.ca>
2017-01-12 12:51:36 -0500

    test

--- cool.js
+++ cool.js
@@ -1,12 +1,13 @@
 "use strict";
 var page = require('webpage').create();
-page.open('https://www.ic.gc.ca/auth/app/scr/ic/idm/login?lang=eng&goto=https://www.nuans.com/auth/app/scr/corp/nuans/member/order.html', function(status) {
-  console.log(status)
-  page.includeJs("https://www.ic.gc.ca/eic/home.nsf/js/webtrends.js", function() {
-    var title = page.evaluate(function() {
+
+page.includeJs("https://www.ic.gc.ca/eic/home.nsf/js/webtrends.js", function() {
+	page.open('https://www.ic.gc.ca/auth/app/scr/ic/idm/login?lang=eng&goto=https://www.nuans.com/auth/app/scr/corp/nuans/member/order.html', function(status) {
+		console.log(status)
+		var title = page.evaluate(function() {
       return jQuery('title').text().trim()
     });
     console.log(title)
     phantom.exit();
-  })
-});
+	})
+})
