0be5d809b665bb03d7af4e179aa1be3172e505b8
Molly Luo <molly@lawscout.ca>
2017-01-16 14:16:31 -0500

    - test

--- slimer-test.js
+++ slimer-test.js
@@ -1,4 +1,12 @@
 var page = require("webpage").create();
+
+// page.open('http://testing-gounds.herokuapp.com/auto/what', function(status) {
+//  var title = page.evaluate(function() {
+//    return document.title;
+//  });
+//  console.log('IP is: ' + title);
+// });
+
 page.open("https://www.ic.gc.ca/auth/app/scr/ic/idm/login?lang=eng&goto=https://www.nuans.com/auth/app/scr/corp/nuans/member/order.html")
 .then(function(status){
      if (status == "success") {
